10465659d2cc6a86519febaa874af425ecd2dc85
Gabriel_Freeze <gabriel.hili.20@um.edu.mt>
2021-11-10 20:08:03 +0100

    Working

--- task1/dagLauncher.cpp
+++ task1/dagLauncher.cpp
@@ -2,12 +2,14 @@
 #include "dag.hpp"
 using namespace std;
 
+/*
+    TODO: Think if Edge should be a class or stay a struct.
+    TODO: Implement checkCycles function.
+    TODO: Implement removeNode and removeEdge functions.
 
+*/
 
 int main() {
-
-    //TODO: Make this an array of edge pointers.
-        Edge<char, int> edges[3];
     
     /*  
     [A]-¹->[B]--²>[C]
@@ -15,34 +17,40 @@ int main() {
     ⌞_____₁______⌟
     */
 
+    Edge<char, int>* edges[3];
+
+    Edge<char, int> edge1;
+    Edge<char, int> edge2;
+    Edge<char, int> edge3;
+
+    edges[0] = &edge1;
+    edges[1] = &edge2;
+    edges[2] = &edge3;
+
+
     Node<char> A;
     Node<char> B;
     Node<char> C;
+
 
     A.val = 'A';
     B.val = 'B';
     C.val = 'C';
 
-    //TODO: Make edges dynamically allocated.
-        // edges[0].weight = 1;
-        // edges[0].from = &A;
-        // edges[0].to = &B;
 
-        // edges[1].weight = 2;
-        // edges[1].from = &B;
-        // edges[1].to = &C;
+    edges[0]->weight = 1;
+    edges[0]->from = &A;
+    edges[0]->to = &B;
 
-        // edges[2].weight = 1;
-        // edges[2].from = &C;
-        // edges[2].to = &A;
+    edges[1]->weight = 2;
+    edges[1]->from = &B;
+    edges[1]->to = &C;
+
+    edges[2]->weight = 1;
+    edges[2]->from = &C;
+    edges[2]->to = &A;
 
     
-    Dag<char, int>* dag = new Dag<char, int>(&edges, 3);
-    
-    // set<Edge<char, int>*> edges2 = dag->getEdges();
-
-    // for (auto it = edges2.begin(); it != edges2.end(); it++) {
-    //     cout << '['<< (*it)->from << ']' << '--' << (*it)->weight << '>' << '[' << (*it)->to << ']' << endl;
-    // }
+    Dag<char, int>* dag = new Dag<char, int>(edges, 3);
 
 }
